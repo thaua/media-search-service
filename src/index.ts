bd2fbcfde2226c3a6f18095ba32c861e50a66a8a
Thauã Silveira <thaua@outlook.com>
2023-10-31 15:29:57 -0300

    Fix image. Husky. Scripts.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import express, { Request, Response } from 'express';
+import Server from './infrastructure/web/server';
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello!');
-});
-
-app.listen(port, () => {
-  console.info(`Server running at http://localhost:${port}`);
-});
+new Server(process.env.PORT || '3000').start();
---
+++ src/infrastructure/web/server.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import * as core from 'express-serve-static-core';
+import http from 'http';
+
+export default class Server {
+  private readonly app: core.Express = express();
+
+  constructor(private readonly port: string) {
+    this.app.get('/', (req: Request, res: Response) => {
+      res.send('Hello!');
+    });
+  }
+
+  start(): void {
+    this.app.listen(this.port, () => {
+      console.info(`Server running at http://localhost:${this.port}`);
+    });
+  }
+}
